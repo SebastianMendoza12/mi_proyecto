e1b57c6fa0435ee44f358d4ca2d79b0114c1ce6b
SebastianMendoza12 <sebas1093432545@gmail.com>
2025-10-03 20:40:02 -0500

    Update models.py

--- usuarios/models.py
+++ usuarios/models.py
@@ -5,4 +5,6 @@ class CustomUser(AbstractUser):
     telefono = models.CharField(max_length=20, blank=True, null=True)
     rol = models.CharField(max_length=20, choices=[("admin", "Admin"), ("cliente", "Cliente")], default="cliente")
 
+    def __str__(self):
+        return self.username
 # Create your models here.
