180c767c4155b4ae869e6b5b4062d7eaaa51b15e
SebastianMendoza12 <sebas1093432545@gmail.com>
2025-10-04 19:03:46 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import Register from "./components/Register";
-import Login from "./components/Login";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
