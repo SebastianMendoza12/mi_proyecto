557a9b755ff89bbfa04f5ce8a0160123d2c0a449
SebastianMendoza12 <sebas1093432545@gmail.com>
2025-10-04 22:53:08 -0500

    Update settings.py

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=True, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['fastfood-fapu.onrender.com', 'localhost', '127.0.0.1']
 
