807e1fc77ea3625085ff84641058ff8a94acc040
SebastianMendoza12 <sebas1093432545@gmail.com>
2025-10-04 21:17:52 -0500

    Update settings.py

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -13,7 +13,6 @@ import os
 from pathlib import Path
 from decouple import config
 import dj_database_url
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,9 +86,15 @@ WSGI_APPLICATION = 'mi_proyecto.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
