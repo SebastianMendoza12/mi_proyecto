8363e7db97975096dfce7cd1aab0a0408a4b5793
SebastianMendoza12 <sebas1093432545@gmail.com>
2025-10-25 17:59:52 -0500

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -44,10 +44,12 @@ function Login() {
       <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
       
         {/* --- LADO IZQUIERDO --- */}
-        <div class="flex-1 bg-indigo-100 text-center hidden lg:flex">
-            <div class="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
-                style="background-image: url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg');">
-            </div>
+        <div className="flex-1 bg-indigo-100 text-center hidden lg:flex items-center justify-center">
+          <img
+            src={logo} // tu imagen local
+            alt="Imagen de fondo personalizada"
+            className="max-w-[80%] max-h-[80%] object-contain rounded-xl shadow-lg"
+          />
         </div>
       </div>
     </div>
