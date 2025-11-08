f05752bb8d7837da4f9a2b0577705add9e06c43c
SebastianMendoza12 <sebas1093432545@gmail.com>
2025-11-08 12:53:53 -0500

    Update views.py

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny 
 from django.contrib.auth import get_user_model
 from rest_framework import status
 import re
@@ -7,6 +8,7 @@ import re
 User = get_user_model()
 
 class RegisterView(APIView):
+    permission_classes = [AllowAny]
     def post(self, request):
         username = request.data.get("username")
         password = request.data.get("password")
