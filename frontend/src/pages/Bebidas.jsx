d58f35575f859977b35d76dd68dfa2070f2cb5df
AndreIng16 <andresuarez0816@gmail.com>
2025-12-01 22:46:44 -0500

    .

--- frontend/src/pages/Bebidas.jsx
+++ frontend/src/pages/Bebidas.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/LogotipoProyecto.png";
+import { getProductos } from "../services/api";
 
 export default function Bebidas() {
   const [searchQuery, setSearchQuery] = useState("");
